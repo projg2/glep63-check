10a089bed8aa8f1ba8fc0c80193a5ec42cf7cdfc
Michał Górny <mgorny@gentoo.org>
2018-08-01 10:11:01 +0200

    Add a setup.py for easy installation

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# (C) 2018 Michał Górny <mgorny@gentoo.org>
+# Released under the terms of the 2-clause BSD license.
+
+from distutils.core import setup
+
+
+setup(
+    name='glep63-check',
+    version='4',
+    author='Michał Górny',
+    author_email='mgorny@gentoo.org',
+    url='http://github.com/mgorny/glep63-verify',
+
+    scripts=['glep63-check'],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Security :: Cryptography'
+    ]
+)
